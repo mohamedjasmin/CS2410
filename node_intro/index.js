917d7d86d09f3468c8d534603bc4ecd590b2c8e7
mohamedjasmin <mohamedj@aston.ac.uk>
2019-04-18 15:52:02 +0100

    worked until step 3

--- node_intro/index.js
+++ node_intro/index.js
@@ -1,1 +1,15 @@
-console.log('index.js executing');
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+  res.send('Hello, World!');
+});
+
+app.post('/',function(req,res){
+  res.status(405).send('error');
+});
+
+var port = 3000;
+app.listen(port,function(){
+  console.log('Listening on port ' + port);
+});
